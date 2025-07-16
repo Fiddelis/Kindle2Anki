46fdfda669cf26d46afff7377ff980d43c2e13f9
Fiddelis <lucas.ruan@ges.inatel.br>
2025-07-16 01:06:30 -0300

    feat: enhance BooksPage with lookup functionality and add Separator component

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,25 +1,53 @@
 "use client";
 import { openDbFromBlob, searchTableClient } from "@/lib/searchKindle";
-import { KindleBookInfo } from "@/lib/types";
+import { KindleBookInfo, LookupWithWord } from "@/lib/types";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { DataTable } from "./components/data-table";
+import { DataTableBooks } from "./components/data-table-books";
 import { columnsBooks } from "./components/columns-books";
+import { columnsLookupWithWord } from "./components/columns-words";
+import { DataTableWords } from "./components/data-table-words";
+import { Separator } from "@/components/ui/separator";
 
 export default function BooksPage() {
   const params = useSearchParams();
   const blobUrl = params.get("fileUrl")!;
   const [books, setBooks] = useState<KindleBookInfo[] | null>(null);
+  const [lookupsWithWords, setLookupsWithWords] = useState<
+    LookupWithWord[] | null
+  >(null);
+
+  async function fetchLookupWithWord(bookId: string[]) {
+    const db = await openDbFromBlob(blobUrl);
+    const rows = searchTableClient<LookupWithWord>(
+      db,
+      "l.id AS lookup_id, l.book_key, l.usage, l.timestamp AS lookup_timestamp, w.id AS word_id, w.word, w.stem, w.lang AS word_lang, w.timestamp AS word_timestamp",
+      "LOOKUPS AS l JOIN WORDS AS w ON l.word_key = w.id",
+      { book_key: bookId }
+    );
+
+    setLookupsWithWords(rows);
+  }
+
+  async function fetchBooks() {
+    const db = await openDbFromBlob(blobUrl);
+    const rows = searchTableClient<KindleBookInfo>(db, "*", "BOOK_INFO", {});
+
+    setBooks(rows);
+  }
+
+  function handleSelectionChange(ids: string[]) {
+    if (ids.length === 0) return;
+
+    fetchLookupWithWord(ids);
+  }
+
+  useEffect(() => {
+    console.log("Lookups with words:", lookupsWithWords);
+  }, [lookupsWithWords]);
 
   useEffect(() => {
     if (!blobUrl) return;
-
-    async function fetchBooks() {
-      const db = await openDbFromBlob(blobUrl);
-      const rows = searchTableClient<KindleBookInfo>(db, "BOOK_INFO", {});
-
-      setBooks(rows);
-    }
 
     fetchBooks();
   }, [blobUrl]);
@@ -38,7 +66,22 @@ export default function BooksPage() {
         Select the books
       </div>
       <div className="container mx-auto px-10">
-        <DataTable columns={columnsBooks} data={books} />
+        <DataTableBooks
+          columns={columnsBooks}
+          data={books}
+          onSelectionChange={handleSelectionChange}
+        />
+      </div>
+      <Separator className="my-10" />
+
+      <div className="w-full flex align-center justify-center text-4xl my-4">
+        Words in selected books
+      </div>
+      <div className="container mx-auto px-10">
+        <DataTableWords
+          columns={columnsLookupWithWord}
+          data={lookupsWithWords ?? []}
+        />
       </div>
     </>
   );
