99d14db1a75401f8126fef7c572c997176acb95c
Fiddelis <lucas.ruan@ges.inatel.br>
2025-08-07 00:23:21 -0300

    feat: add ts-jest for TypeScript support in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,8 @@
 
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -14,6 +16,9 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
