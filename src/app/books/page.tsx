db266f68003612148b0fe222ac3d583f1e582ad4
Fiddelis <lucas.ruan@ges.inatel.br>
2025-08-12 13:37:01 -0300

    feat: wrap PageClient in Suspense

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from 'react';
-import SelectBooks from './components/select-books';
-import ExampleCards from './components/anki-cards';
 import PageClient from './page-client';
 
 export default function BooksPage() {
   return (
     <div className="flex justify-center ">
-      <PageClient />
+      <Suspense>
+        <PageClient />
+      </Suspense>
     </div>
   );
 }
